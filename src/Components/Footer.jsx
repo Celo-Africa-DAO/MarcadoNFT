3f4012d92ad2aafa0fc6c913786c9af0975a48ea
Muhindo-Galien <muhindogalien512@gmail.com>
2024-12-12 09:28:05 -0700

    upated footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,17 +6,20 @@ const Footer = () => {
 
     const date = new Date().getFullYear();
     return (
-    <div className='footer max-w-6xl my-4 mx-auto  flex flex-col md:flex-row justify-between  font-globalFont text-gray-400 '>
-      <h2> &copy; {date} Marcado NFTMarketplace </h2>
-      <h4 className='flex items-center'>Designed By Galien Dev
-       <a href="https://twitter.com/GalienMuhindo" target='_blank'
-       className='text-2xl ml-1 text-gradient'>
-          <AiOutlineTwitter />
-        </a>
-       </h4>
-      
-    </div>
-  )
+      <div className="footer max-w-6xl my-4 mx-auto  flex flex-col md:flex-row justify-between  font-globalFont text-gray-400 ">
+        <h2> &copy; {date} Marcado NFTMarketplace </h2>
+        <h4 className="flex items-center">
+          Designed By Celo Africa DAO
+          <a
+            href="https://x.com/CeloAfricaDao"
+            target="_blank"
+            className="text-2xl ml-1 text-gradient"
+          >
+            <AiOutlineTwitter />
+          </a>
+        </h4>
+      </div>
+    );
 }
 
 export default Footer
